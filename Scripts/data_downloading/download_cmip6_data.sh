2417e93d797d97ad92e3d0d6989b8920b8e969fd
Bernat Jiménez <bernatje92@gmail.com>
2024-02-13 11:56:12 +0100

    fixed some error

--- Scripts/data_downloading/download_cmip6_data.sh
+++ Scripts/data_downloading/download_cmip6_data.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-exp=historical
+#before downloading the data you can check which models are availabel by exectuting the following command.
+#e.g. for relative humidity (hur) monthly means, one realization
+#acccmip6 -o S -e historical -v hur -r atmos -f mon -rlzn 1 
 
+#CMIP6 experiment
+exp=historical
 # Set the base directory
-base_dir="/pool/usuarios/bernatj/CMIP6/${exp}" 
+base_dir="/pool/datos/modelos/cmip6/${exp}" 
 # List of models
-models="CESM2-WACCM-FV2 KACE-1-0-G CAMS-CSM1-0 MIROC-ES2L"
+models="E3SM-2-0-NARRM EC-Earth3-AerChem CNRM-ESM2-1 NorESM2-MM CESM2-WACCM IITM-ESM EC-Earth3 IPSL-CM6A-LR-INCA NESM3 CMCC-CM2-HR4 \
+        CAMS-CSM1-0 E3SM-2-0 ACCESS-CM2 CNRM-CM6-1 ACCESS-ESM1-5 BCC-CSM2-MR CESM2-FV2 SAM0-UNICON GISS-E2-2-G TaiESM1 CanESM5 INM-CM5-0 \
+        FGOALS-g3 AWI-CM-1-1-MR CESM2 HadGEM3-GC31-MM UKESM1-0-LL FIO-ESM-2-0 MIROC6 MPI-ESM-1-2-HAM GISS-E2-2-H MIROC-ES2H MPI-ESM2-0 \
+        GISS-E2-1-G BCC-ESM1 E3SM-1-1"
+
 # List of variables separated by space
 variables="hur"     
 
@@ -23,6 +31,6 @@ for model in $models; do
     # Iterate over each variable
     for variable in $variables; do
         # Download data using acccmip6
-        acccmip6 -o D -m $model -e historical -v $variable -r atmos -f mon -rlzn 1 -d "$directory"
+        acccmip6 -o D -m $model -e historical -v $variable -r atmos -f mon -rlzn 1 -dir "$directory"
     done
 done
